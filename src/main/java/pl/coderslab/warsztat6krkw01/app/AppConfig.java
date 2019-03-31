027949e8683b23acd4edf334ed7b6156cca6709f
rafalcl <rafalclab@gmail.com>
2019-03-31 13:02:49 +0200

    Initialize Spring with hibernate project

---
+++ src/main/java/pl/coderslab/warsztat6krkw01/app/AppConfig.java
@@ -0,0 +1,66 @@
+package pl.coderslab.warsztat6krkw01.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleContextResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.persistence.EntityManagerFactory;
+import javax.validation.Validator;
+import java.util.Locale;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"pl.coderslab.warsztat6krkw01"})
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "pl.coderslab.warsztat6krkw01.repository")
+public class AppConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
+        emfb.setPersistenceUnitName("warsztat6krkw01");
+        return emfb;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager tm = new JpaTransactionManager(emf);
+        return tm;
+    }
+
+    @Bean
+    public Validator validator() {
+        return new LocalValidatorFactoryBean();
+    }
+
+    @Bean(name = "localeResolver")
+    public LocaleContextResolver getLocaleContextResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("pl", "PL"));
+        return localeResolver;
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+//        registry.addConverter(stringAuthorConverter());
+    }
+}
---
+++ target/warsztat6krkw01-1.0-SNAPSHOT/WEB-INF/views/home/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Edu
+  Date: 31.03.2019
+  Time: 12:59
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Twitter App</title>
+</head>
+<body>
+    <div>You are at home. :)</div>
+</body>
+</html>
